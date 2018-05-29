a82426e83ce0d6a2df67f3aeb21835b08dbefe75
markforest <toxerius7@gmail.com>
2018-05-29 15:17:53 +0300

    Pattern Command is finished

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp1.Clients;
+using ConsoleApp1.Commands;
+using ConsoleApp1.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteControl remoteControl = new RemoteControl();
+            //GarageDoor
+            GarageDoor garageDoor = new GarageDoor();
+            GarageDoorCloseCommand garageDoorCloseCommand = new GarageDoorCloseCommand(garageDoor);
+            GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
+            Light light = new Light();
+            LightOnCommand lightOnCommand = new LightOnCommand(light);
+            LightOffCommand lightOffCommand = new LightOffCommand(light);
+
+
+
+            remoteControl.SetCommand(0, garageDoorOpenCommand, garageDoorCloseCommand);
+            remoteControl.SetCommand(1, lightOnCommand, lightOffCommand);
+
+
+
+            remoteControl.OnButtonWasPushed(0);
+            remoteControl.OnButtonWasPushed(1);
+
+            Console.WriteLine(remoteControl);
+
+            remoteControl.OffButtonWasPushed(0);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
